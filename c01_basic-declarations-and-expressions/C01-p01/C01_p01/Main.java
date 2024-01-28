d1bd65c2067d951bc10ed4364f51db8d9dec5339
DeedeeDL <diana_girl_89@yahoo.com>
2024-01-28 21:56:01 +0200

    C01-p05
    Exercise 05 from C01

---
+++ c01_basic-declarations-and-expressions/C01-p01/C01_p01/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        /* Write a Java program to print your name, date of birth, and mobile number.
+        Expected Output:
+        Name   : Alexandra Abramov
+        DOB    : July 14, 1975
+        Mobile : 99-9999999999 */
+
+        System.out.println("Name : Dragoi Diana");
+        System.out.println("DOB: Sept 16 1989");
+        System.out.println("Mobile: 00000000");
+    }
+}
