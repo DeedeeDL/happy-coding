6fedc2c73db71a30953e4371044c46add0ebc9e3
DeedeeDL <diana_girl_89@yahoo.com>
2024-01-30 22:11:33 +0200

    Create_P03_rectangleperimeterandarea

---
+++ src/main/java/org/example/P03_RectanglePerimeterAndArea.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class P03_RectanglePerimeterAndArea {
+    public static void main(String[] args) {
+           /* Write a C program to compute the perimeter and area of a rectangle with a
+    height of 7 inches and width of 5 inches.
+    Expected Output:
+    Perimeter of the rectangle = 24 inches
+    Area of the rectangle = 35 square inches */
+
+        int height = 7;
+        int width = 5;
+        int perimeter = (height + width) * 2;
+        int area = height * width;
+
+        System.out.println("Perimeter of the rectangle = " + perimeter + " inches");
+        System.out.println("Area of the rectangle = " + area + " inches");
+    }
+}
