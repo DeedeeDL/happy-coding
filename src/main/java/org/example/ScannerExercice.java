d85831ca1474ef6c62b480fb87c60f7c6ce38dce
DeedeeDL <diana_girl_89@yahoo.com>
2024-01-31 14:52:06 +0200

    CreateMyFirstScanner

---
+++ src/main/java/org/example/ScannerExercice.java
@@ -0,0 +1,10 @@
+package org.example;
+import java.util.Scanner;
+
+public class ScannerExercice {
+    public static void main(String[] args) {
+        Scanner test = new Scanner(System.in);
+        System.out.println(test.nextLine());
+    }
+
+}
