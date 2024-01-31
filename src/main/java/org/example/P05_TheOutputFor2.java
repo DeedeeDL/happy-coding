4ed084ed3a09a383999a776060863a521b8b1c77
DeedeeDL <diana_girl_89@yahoo.com>
2024-01-31 13:26:15 +0200

    Create P05_TheOutputFor2

---
+++ src/main/java/org/example/P05_TheOutputFor2.java
@@ -0,0 +1,11 @@
+package org.example;
+/* what is the output for System.out.println((2+3)4)*/
+public class P05_TheOutputFor2 {
+    public static void main(String[] args) {
+        int calcul = (2+3)*4;
+        System.out.println(calcul);
+        /* the output will be 20 because we calculate first the addition because it is between brakets, and after that we multiplicate the result with 4 */
+
+
+    }
+}
