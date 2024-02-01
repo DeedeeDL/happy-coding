c306fe1881bbbb59e0089916745efc28974f40f0
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-01 14:26:34 +0200

    Update_ P01_OperationsPriority.
    Changing the class and branch name

--- src/main/java/org/example/P01_OperationsPriority.java
+++ src/main/java/org/example/P01_OperationsPriority.java
@@ -1,6 +1,6 @@
 package org.example;
 /*What is the output for System.out.println(-6+4*2)*/
-public class P04_TheOutputFor {
+public class P01_OperationsPriority {
     public static void main(String[] args) {
         System.out.println(-6+4*2);
         /*The output will be 2 because of operations priority:first is the multiplication, the second is the addition and then the substraction*/
