6633ce574acfd3edcf31d9bc9581b137193fc4b5
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-05 14:02:28 +0200

    Create P_05

---
+++ src/main/java/org/example/P05_EvenOrOddNumber.java
@@ -0,0 +1,8 @@
+package org.example;
+import java.util.Scanner;
+public class P05_EvenOrOddNumber {
+    public static void main(String[] args) {
+        Scanner myNumber = new Scanner(System.in);
+        int num1 = myNumber.nextInt();
+    }
+}
