912cf1e1a2f2b51d361e5164618204e4367b71c9
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-05 14:11:24 +0200

    Update P05

--- src/main/java/org/example/P05_EvenOrOddNumber.java
+++ src/main/java/org/example/P05_EvenOrOddNumber.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class P05_EvenOrOddNumber {
     public static void main(String[] args) {
         Scanner myNumber = new Scanner(System.in);
+        System.out.println("Enter your number:");
         int num1 = myNumber.nextInt();
+
+        if ( num1%2==0 ){
+            System.out.println("The number " + num1 + " is even");
+        }
+        else {
+            System.out.println("The number " + num1 + " is odd;");}
+
+
+
     }
 }
