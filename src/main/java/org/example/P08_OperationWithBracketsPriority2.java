6a0773e317d67f9d7de0e91e4ac932fe89a67576
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-05 15:25:50 +0200

    Update P08

--- src/main/java/org/example/P08_OperationWithBracketsPriority2.java
+++ src/main/java/org/example/P08_OperationWithBracketsPriority2.java
@@ -3,6 +3,6 @@ package org.example;
 public class P08_OperationWithBracketsPriority2 {
     public static void main(String[] args) {
         System.out.println(7+(3*5)*(4/2)+(5%3)-1);
-        //The operations priority will b : opeations in brackets, *,/,%,+, so : 7+15*2+2-1 = 7+30+1= 38
+        //The operations priority will be : opeations in brackets, *,/,%,+,-, so : 7+15*2+2-1 = 7+30+1= 38
     }
 }
