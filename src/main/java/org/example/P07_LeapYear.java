a2c8edcb15d47aadc510e2900d3a6b3b0639d189
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-06 15:13:27 +0200

    Update P07

--- src/main/java/org/example/P07_LeapYear.java
+++ src/main/java/org/example/P07_LeapYear.java
@@ -1,7 +1,16 @@
 package org.example;
-
+import java.util.Scanner;
 public class P07_LeapYear {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your year:");
+        int myYear = scanner.nextInt();
+        scanner.close();
+
+        if(myYear%4 == 0 && myYear % 100 ==0 && myYear%400 == 0) {
+            System.out.println(myYear + " is a leap year");
+        }
+        else {
+            System.out.println(myYear + " is not a leap year");}
     }
 }
