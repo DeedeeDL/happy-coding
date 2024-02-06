503f5856147f64c311de56e7b68238f95a871afc
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-06 15:21:05 +0200

    Adding documentaries

--- src/main/java/org/example/P07_LeapYear.java
+++ src/main/java/org/example/P07_LeapYear.java
@@ -2,10 +2,12 @@ package org.example;
 import java.util.Scanner;
 public class P07_LeapYear {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // create the scanner object for an user input
         System.out.println("Enter your year:");
-        int myYear = scanner.nextInt();
+        int myYear = scanner.nextInt(); // create the variable myYear to write the own year
         scanner.close();
+
+       // Using if we calculate if an year is a leap year or not (knowing that a year si a leap year only if it is divided by 4,100 and 400
 
         if(myYear%4 == 0 && myYear % 100 ==0 && myYear%400 == 0) {
             System.out.println(myYear + " is a leap year");
