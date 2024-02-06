d0cbdb5122cdb4f1c9b19023ef9eff32df022eb6
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-06 14:51:32 +0200

    Update P05

--- src/main/java/org/example/P05_EvenOrOddNumber.java
+++ src/main/java/org/example/P05_EvenOrOddNumber.java
@@ -2,9 +2,9 @@ package org.example;
 import java.util.Scanner;
 public class P05_EvenOrOddNumber {
     public static void main(String[] args) {
-        Scanner myNumber = new Scanner(System.in);  // this will allow us to user input the number
+        Scanner scanner = new Scanner(System.in);  // this will allow us to user input the number
         System.out.println("Enter your number:");
-        int num1 = myNumber.nextInt();  // I created a variable named num1
+        int num1 = scanner.nextInt();  // I created a variable named num1
      // I will use if and else to see if the number is odd or even
         if ( num1%2==0 ){
             System.out.println("The number " + num1 + " is an even number");
