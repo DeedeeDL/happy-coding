d26d84c9d4f25af7747e57143c408a1d6d949348
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-06 14:46:52 +0200

    Update P06

--- src/main/java/org/example/P06_MultiplicationTable.java
+++ src/main/java/org/example/P06_MultiplicationTable.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class P06_MultiplicationTable {
     public static void main(String[] args) {
-        Scanner number = new Scanner(System.in);
+        Scanner inputNum = new Scanner(System.in);
         System.out.println("Enter your number: ");
-        int num = number.nextInt();
-        number.close();
+        int num = inputNum.nextInt();
+        inputNum.close();
 
         for (int i = 0; i <= 100; i++) {
             System.out.println(num + "x" + i + "=" + (num * i));
