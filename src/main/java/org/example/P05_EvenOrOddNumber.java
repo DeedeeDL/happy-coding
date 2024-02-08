0246189167d53935785fe80d3ffcaef1ce15ac1d
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-08 19:02:57 +0200

    Update P05

--- src/main/java/org/example/P05_EvenOrOddNumber.java
+++ src/main/java/org/example/P05_EvenOrOddNumber.java
@@ -4,13 +4,13 @@ public class P05_EvenOrOddNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);  // this will allow us to user input the number
         System.out.println("Enter your number:");
-        int num1 = scanner.nextInt();  // I created a variable named num1
+        int number = scanner.nextInt();  // I created a variable named num1
      // I will use if and else to see if the number is odd or even
-        if ( num1%2==0 ){
-            System.out.println("The number " + num1 + " is an even number");
+        if ( number%2==0 ){
+            System.out.println("The number " + number + " is an even number");
         }
         else {
-            System.out.println("The number " + num1 + " is an odd number;");}
+            System.out.println("The number " + number + " is an odd number;");}
 
 
 
