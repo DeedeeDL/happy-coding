04cc353e18b2c91bf4e320c76d7df33a8f72cb8b
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-08 19:16:19 +0200

    Create P11

---
+++ src/main/java/org/example/P11_QuotientAndReminder.java
@@ -0,0 +1,12 @@
+package org.example;
+import java.util.Scanner;
+
+public class P11_QuotientAndReminder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dividend = scanner.nextInt();
+        int divizor = scanner.nextInt();
+        System.out.println("Enter your first number: "+dividend);
+        System.out.println("Enter your divizor: "+divizor);
+    }
+}
