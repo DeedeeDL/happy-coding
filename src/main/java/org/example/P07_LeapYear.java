46a932217bc5f5745b0f58d71359908082a14980
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-08 18:54:22 +0200

    Update P07

--- src/main/java/org/example/P07_LeapYear.java
+++ src/main/java/org/example/P07_LeapYear.java
@@ -7,12 +7,22 @@ public class P07_LeapYear {
         int myYear = scanner.nextInt(); // create the variable myYear to write the own year
         scanner.close();
 
-       // Using if we calculate if an year is a leap year or not (knowing that a year si a leap year only if it is divided by 4,100 and 400
+        // Using if we calculate if an year is a leap year or not (knowing that a year si a leap year only if it is divided by 4,100 and 400
 
-        if(myYear%4 == 0 && myYear % 100 ==0 && myYear%400 == 0) {
-            System.out.println(myYear + " is a leap year");
+        if (myYear % 4 == 0) {
+            if (myYear % 100 == 0) {
+                if (myYear % 400 == 0) {
+                    System.out.println(myYear + " is a leap year");
+                } else {
+                    System.out.println(myYear + " is not a leap year");
+                }
+            } else {
+                System.out.println(myYear + " is a leap year");
+            }
+         }     else {
+            System.out.println(myYear + " is not a leap year");
         }
-        else {
-            System.out.println(myYear + " is not a leap year");}
+
+        }
     }
-}
+
