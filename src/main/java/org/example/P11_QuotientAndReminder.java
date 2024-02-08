e56a8941abc64520df8f5f0c03ccd7073ceeff72
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-08 19:25:31 +0200

    Update P11

--- src/main/java/org/example/P11_QuotientAndReminder.java
+++ src/main/java/org/example/P11_QuotientAndReminder.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 public class P11_QuotientAndReminder {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your first number: ");
         int dividend = scanner.nextInt();
-        int divizor = scanner.nextInt();
-        System.out.println("Enter your first number: "+dividend);
-        System.out.println("Enter your divizor: "+divizor);
+        System.out.println("Enter your divizor: ");
+       int divizor = scanner.nextInt();
+
+        System.out.println("Quotient is: " + (dividend/divizor));
+        System.out.println("Reminder is: " + (dividend%divizor));
     }
 }
