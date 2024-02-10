a2ae9379ea0aa1ef93aeacf03edb02f2b8ab71e6
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-10 18:58:27 +0200

    Update_format

--- src/main/java/org/example/P16_SumOfEvenNumbers.java
+++ src/main/java/org/example/P16_SumOfEvenNumbers.java
@@ -3,11 +3,11 @@ package org.example;
 public class P16_SumOfEvenNumbers {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i=0;i<=10;i++) {
+        for (int i = 0; i <= 10; i++) {
             if (i % 2 == 0) {
                 sum = sum + i;
             }
         }
-        System.out.println(" The sum of the even numbers from 0 to 10 is:" + sum );
+        System.out.println(" The sum of the even numbers from 0 to 10 is:" + sum);
     }
 }
