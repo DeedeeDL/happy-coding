c24da90ba7cda02384ca1aa05a01233a3fdf1164
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-10 20:13:36 +0200

    Update P19

--- src/main/java/org/example/P19_rectanglePerimeter.java
+++ src/main/java/org/example/P19_rectanglePerimeter.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class P19_rectanglePerimeter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the width of the rectangle: ");
+        double width = scanner.nextDouble();
+        System.out.println("Enter the height of the rectangle: ");
+        double height = scanner.nextDouble();
+
+        System.out.println("The perimeter of the rectangle is : " + 2 * (width + height));
+    }
 }
