003d62593c39f6b598f93db6b73911ef966de9a0
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-12 21:47:50 +0200

    Update_29

--- src/main/java/org/example/P29_SquareOfANumber.java
+++ src/main/java/org/example/P29_SquareOfANumber.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class P29_squareOfANumber {
+public class P29_SquareOfANumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your number: ");
