647178c3b6fad840af28df38c0bcaa0dd3c5b1d3
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-12 21:49:21 +0200

    UpdateP30

--- src/main/java/org/example/P30_CubeOfANumber.java
+++ src/main/java/org/example/P30_CubeOfANumber.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class P30_cubeOfANumber {
+public class P30_CubeOfANumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your number:");
