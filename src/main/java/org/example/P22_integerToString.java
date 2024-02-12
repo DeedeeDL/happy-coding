f29980d307d2bb411587bfdda7b08bf439a020f9
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-12 21:46:26 +0200

    UpdateP22

--- src/main/java/org/example/P22_integerToString.java
+++ src/main/java/org/example/P22_integerToString.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class P22_integerToString {
+public class P22_IntegerToString {
     public static void main(String[] args) {
         int num = 1989;
         String text = String.valueOf(num);
