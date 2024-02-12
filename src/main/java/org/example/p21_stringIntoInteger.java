6791c36630a8fa1b93b49a5f96aaae8bc1af1b66
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-12 11:25:34 +0200

    CreateP21

---
+++ src/main/java/org/example/p21_stringIntoInteger.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class p21_stringIntoInteger {
+    public static void main(String[] args) {
+        String text = "1234";
+        int numar = Integer.valueOf(text);
+
+        System.out.println("The value of the integer is : " + numar);
+    }
+}
