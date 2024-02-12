48ed40d11ce01e224b367bfcdf09a295e58af7f0
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-12 11:52:40 +0200

    Create P29

---
+++ src/main/java/org/example/P29_squareOfANumber.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class P29_squareOfANumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+        int square = num * num;
+        System.out.println("The square of your number is: " + square);
+    }
+}
