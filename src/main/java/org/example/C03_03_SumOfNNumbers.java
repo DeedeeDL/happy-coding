79da4fbe2457057dd026a3afa419f9605e29d59f
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-15 19:57:23 +0200

    Update

--- src/main/java/org/example/C03_03_SumOfNNumbers.java
+++ src/main/java/org/example/C03_03_SumOfNNumbers.java
@@ -3,13 +3,13 @@ package org.example;
 public class C03_03_SumOfNNumbers {
     public static void main(String[] args) {
         int n = 7;
-        int suma = 0;
+        int sum = 0;
 
 
         for (int i = 0; i <= n; i++) {
-            suma = suma + i;
+            sum = sum + i;
 
         }
-        System.out.println("Suma numerelor este de: " + suma);
+        System.out.println("Suma numerelor este de: " + sum);
     }
 }
