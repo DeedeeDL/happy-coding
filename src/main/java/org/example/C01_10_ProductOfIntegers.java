bfe1fe9f1629d52dc45809723e49d9a5a99be6dc
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-15 19:53:09 +0200

    Update

--- src/main/java/org/example/C01_10_ProductOfIntegers.java
+++ src/main/java/org/example/C01_10_ProductOfIntegers.java
@@ -10,5 +10,6 @@ public class C01_10_ProductOfIntegers {
         System.out.println("Enter your second number: ");
         int num2 = scanner.nextInt();
         System.out.println("The product of your numbers is: " + (num1 * num2));
+        
     }
 }
