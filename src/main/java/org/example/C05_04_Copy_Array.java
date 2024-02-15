ce01ff2d83cce0895af8362c67ef2e4545e61bd7
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-15 20:08:34 +0200

    Update

--- src/main/java/org/example/C05_04_Copy_Array.java
+++ src/main/java/org/example/C05_04_Copy_Array.java
@@ -2,25 +2,24 @@ package org.example;
 
 public class C05_04_Copy_Array {
     public static void main(String[] args) {
-        int[] elements1 = new int[]{1, 9, 8, 9};
-        //Create another array arr2 with size of arr1
-        int elements2[] = new int[elements1.length];
+        int[] originalArray = new int[]{1, 9, 8, 9};
+
+        int[] newArray = new int[originalArray.length];
         //Copying all elements of one array into another
-        for (int i = 0; i < elements1.length; i++) {
-            elements2[i] = elements1[i];
+        for (int i = 0; i < originalArray.length; i++) {
+            newArray[i] = originalArray[i];
         }
 
         System.out.println("Elements of original array: ");
-        for (int i = 0; i < elements1.length; i++) {
-            System.out.print(elements1[i] + " ");
+        for (int i = 0; i < originalArray.length; i++) {
+            System.out.print(originalArray[i] + " ");
         }
 
         System.out.println();
 
-        //Displaying elements of array arr2
         System.out.println("Elements of new array: ");
-        for (int i = 0; i < elements2.length; i++) {
-            System.out.print(elements2[i] + " ");
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.print(newArray[i] + " ");
         }
     }
 }
