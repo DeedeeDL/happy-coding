293f12c99d725bd41c9105976f5425b088eb54a7
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-15 19:56:06 +0200

    Update

--- src/main/java/org/example/C03_02_SumOfNumbers.java
+++ src/main/java/org/example/C03_02_SumOfNumbers.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class C03_02_SumOfNumbers {
     public static void main(String[] args) {
-        int suma = 0;
+        int sum = 0;
 
         for (int i=0; i<=10; i++){
-            suma = suma + i;
+            sum = sum + i;
         }
         System.out.println("Suma primelor 10 numere este: " + suma);
     }
