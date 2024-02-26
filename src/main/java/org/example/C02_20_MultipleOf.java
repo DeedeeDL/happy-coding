ead54f73ad5f424021d07e20fd890f18326a8fdb
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-26 11:51:19 +0200

    Create C02_20

---
+++ src/main/java/org/example/C02_20_MultipleOf.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class C02_20_MultipleOf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int number = scanner.nextInt();
+
+        if (number %3 == 0 ^ number %7 ==0){
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
+
+        }
+    }
+
+
+}
