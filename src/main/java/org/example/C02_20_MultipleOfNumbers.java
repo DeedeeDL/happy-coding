eb4a7a6ed7b668e2b1bbf2f390114687c3c1b36b
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-27 19:17:07 +0200

    UpdateC02_20

--- src/main/java/org/example/C02_20_MultipleOfNumbers.java
+++ src/main/java/org/example/C02_20_MultipleOfNumbers.java
@@ -2,13 +2,13 @@ package org.example;
 
 import java.util.Scanner;
 
-public class C02_20_MultipleOf {
+public class C02_20_MultipleOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your number: ");
         int number = scanner.nextInt();
 
-        if (number % 3 == 0 ^ number % 7 == 0) {
+        if (number % 3 == 0 || number % 7 == 0) {
             System.out.println(true);
         } else {
             System.out.println(false);
