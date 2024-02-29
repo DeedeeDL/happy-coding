d77a3a4b6bd69170305736144ecaa744a0e3cf7c
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-29 18:22:14 +0200

    Update C01_08_DaysIntoYearsWeeksAndDays.java

---
+++ src/main/java/org/example/C01_08_DaysIntoYearsWeeksAndDays.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class C01_08_DaysIntoYearsWeeksAndDays {
+    public static void main(String[] args) {
+        // number of days - 1329;
+        int days = 1329;
+        int years =
+    }
+}
