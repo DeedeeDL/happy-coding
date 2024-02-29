3ba49671c4dde50d49c60c7163f98520c45e1445
DeedeeDL <diana_girl_89@yahoo.com>
2024-02-29 18:40:09 +0200

    Update

--- src/main/java/org/example/C01_08_DaysIntoYearsWeeksAndDays.java
+++ src/main/java/org/example/C01_08_DaysIntoYearsWeeksAndDays.java
@@ -4,11 +4,11 @@ public class C01_08_DaysIntoYearsWeeksAndDays {
     public static void main(String[] args) {
         // number of days - 1329;
         int days = 1329;
-        int years = days/365;
-        int weeks = (days % 365)/7;
+        int years = days / 365;
+        int weeks = (days % 365) / 7;
         System.out.println(years);
         System.out.println(weeks);
-        int daysremained = days - ((years*365)+(weeks*7));
+        int daysremained = days - ((years * 365) + (weeks * 7));
         System.out.println(daysremained);
 
     }
