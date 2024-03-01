c6e9af6dccc68643bcc980f36dfc08b3c31a7fc0
DeedeeDL <diana_girl_89@yahoo.com>
2024-03-01 12:12:28 +0200

    Update C03_04

--- src/main/java/org/example/C03_04_SumAndAverageOfNumbers.java
+++ src/main/java/org/example/C03_04_SumAndAverageOfNumbers.java
@@ -6,11 +6,13 @@ public class C03_04_SumAndAverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your 10 numbers: ");
-        int number = scanner.nextInt();
+
         int sum = 0;
 
         for (int i = 1; i <= 10; i++) {
-            sum = sum + i;
+            int numbers = scanner.nextInt();
+            sum += numbers;
+
         }
         double average = sum / 10;
 
