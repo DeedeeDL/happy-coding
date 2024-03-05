c24ab50825fdbe6e8d1188efbcb24b6c093d3688
DeedeeDL <diana_girl_89@yahoo.com>
2024-03-05 11:57:33 +0200

    Create C03_12

---
+++ src/main/java/org/example/C03_12__PatternAngledTriangleIncreasingNumber.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class C03_12__PatternAngledTriangleIncreasingNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        int k = 1;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(k++ + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
