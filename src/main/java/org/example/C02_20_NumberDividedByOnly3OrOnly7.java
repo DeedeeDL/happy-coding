d440758e4bde8d9fe7e93e5a627eee37596c850e
DeedeeDL <diana_girl_89@yahoo.com>
2024-03-05 11:03:31 +0200

    Update

--- src/main/java/org/example/C02_20_NumberDividedByOnly3OrOnly7.java
+++ src/main/java/org/example/C02_20_NumberDividedByOnly3OrOnly7.java
@@ -2,19 +2,19 @@ package org.example;
 
 import java.util.Scanner;
 
-public class C02_20_MultipleOfNumbers {
+public class C02_20_NumberDividedByOnly3OrOnly7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your number: ");
         int number = scanner.nextInt();
 
-        if (number % 3 == 0 || number % 7 == 0) {
-            System.out.println(true);
-        } else {
+        if (number % 3 == 0 && number % 7 == 0) {
             System.out.println(false);
-
+        } else if (number % 3 == 0 || number % 7 == 0) {
+            System.out.println(true);
+        }
+        else {
+            System.out.println(false);
         }
     }
-
-
 }
