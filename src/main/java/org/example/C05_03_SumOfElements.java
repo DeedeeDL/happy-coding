003eccd161200e4a1b635abdb823a75b8d9b7637
DeedeeDL <diana_girl_89@yahoo.com>
2024-03-07 17:24:00 +0200

    Update

--- src/main/java/org/example/C05_03_SumOfElements.java
+++ src/main/java/org/example/C05_03_SumOfElements.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class C05_03_SumOfElements {
     public static void main(String[] args) {
-        int [] x = {2,5,9,12};
+        int[] x = {2, 5, 9, 12};
         int sum = 0;
-        for (int a:x){
-            sum = sum+a;
+        for (int a : x) {
+            sum = sum + a;
         }
         System.out.println("The sum of the elements from the array is: " + sum);
     }
