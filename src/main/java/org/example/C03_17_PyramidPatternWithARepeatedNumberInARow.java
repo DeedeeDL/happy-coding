77576b4ac9f684a4def25febad13a2efcd217e20
DeedeeDL <diana_girl_89@yahoo.com>
2024-03-14 14:59:25 +0200

    Update_inspir

--- src/main/java/org/example/C03_17_PyramidPatternWithARepeatedNumberInARow.java
+++ src/main/java/org/example/C03_17_PyramidPatternWithARepeatedNumberInARow.java
@@ -23,10 +23,3 @@ public class C03_17_PyramidPatternWithARepeatedNumberInARow {
         }
     }
 }
-
- /* for (int i = 1; i <= rows; i++) {
-          for (int j = 1; j <= i; j++) {
-          System.out.print(i + " ");
-          }
-          System.out.println();
-          } */
