726b83a800cb362cb7a7bf15b1ba737ea5ae22c3
DeedeeDL <diana_girl_89@yahoo.com>
2024-03-28 21:32:44 +0200

    Update_reformat

--- src/main/java/org/example/C01_10_MultiplyOfIntegers.java
+++ src/main/java/org/example/C01_10_MultiplyOfIntegers.java
@@ -1,10 +1,10 @@
 package org.example;
 
 public class C01_10_MultiplyOfIntegers {
-    public static int multiply (int num1, int num2){
+    public static int multiply(int num1, int num2) {
         int multiply = num1 * num2;
         return multiply;
     }
-        
+
 
 }
