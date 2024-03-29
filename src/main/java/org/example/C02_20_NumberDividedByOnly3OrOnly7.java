d1778a80614674d8e3b20b9e578370f5b8233124
DeedeeDL <diana_girl_89@yahoo.com>
2024-03-29 12:25:38 +0200

    Create

--- src/main/java/org/example/C02_20_NumberDividedByOnly3OrOnly7.java
+++ src/main/java/org/example/C02_20_NumberDividedByOnly3OrOnly7.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class C02_20_NumberDividedByOnly3OrOnly7 {
-    public static boolean isDivided (int number){
+    public static boolean isDivided(int number) {
         boolean result;
         if (number % 3 == 0 && number % 7 == 0) {
             result = false;
         } else if (number % 3 == 0 || number % 7 == 0) {
-           result = true;
+            result = true;
         } else {
             result = false;
         }
