69733e2ff61da08695501ea3ffcf655d6e217089
DeedeeDL <diana_girl_89@yahoo.com>
2024-04-02 18:08:37 +0300

    update

--- src/main/java/org/example/C01_01_PrintNameDateOfBirtMobileNb.java
+++ src/main/java/org/example/C01_01_PrintNameDateOfBirtMobileNb.java
@@ -3,6 +3,15 @@ package org.example;
 public class C01_01_PrintNameDateOfBirtMobileNb {
     public static String names(String name) {
         String names = "Diana-Lavinia";
-        return names;
+        return names ;
+    }
+    public static String birthdates(String dateofbirth) {
+        String birthdates =dateofbirth;
+        return birthdates;
+    }
+    public static int phone(int phone1) {
+        int phone = phone1;
+        return phone;
+
     }
 }
--- src/test/java/org/example/C01_01_PrintNameDateOfBirtMobileNbTest.java
+++ src/test/java/org/example/C01_01_PrintNameDateOfBirtMobileNbTest.java
@@ -14,5 +14,23 @@ class C01_01_PrintNameDateOfBirtMobileNbTest {
 
 
     }
+    @Test
+    public void name2() {
+
+        String dateOfBirth = "16.09.1989";
+        String names = C01_01_PrintNameDateOfBirtMobileNb.birthdates(dateOfBirth);
+        assertEquals("16.09.1989", dateOfBirth);
+
+
+    }
+    @Test
+    public void phonenb() {
+
+        int phonenumb = 07;
+        int phone = C01_01_PrintNameDateOfBirtMobileNb.phone(phonenumb);
+        assertEquals(07, phone);
+
+
+    }
 
 }
