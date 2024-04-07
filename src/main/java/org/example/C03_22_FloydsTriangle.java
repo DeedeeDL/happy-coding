6edc2abd52a2c9432644a6c37126b47dffdcd865
DeedeeDL <diana_girl_89@yahoo.com>
2024-04-07 20:11:09 +0300

    updated

--- src/main/java/org/example/C03_22_FloydsTriangle.java
+++ src/main/java/org/example/C03_22_FloydsTriangle.java
@@ -7,10 +7,9 @@ public class C03_22_FloydsTriangle {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of rows: ");
         int rows = scanner.nextInt();
-        int number;
-    
-        for (int i=1; i<=rows;i++)
-        {
+        int number = 1;
+
+        for (int i = 1; i <= rows; i++) {
 
             for (int j = 1; j <= i; j++) {
                 System.out.print(number + " ");
