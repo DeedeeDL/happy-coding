ee436fc36d10bc0d6ebaf4150f173e9f16933cd1
DeedeeDL <diana_girl_89@yahoo.com>
2024-04-07 20:02:06 +0300

    Updated

--- src/main/java/org/example/P20_SumOfDigits.java
+++ src/main/java/org/example/P20_SumOfDigits.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 public class P20_SumOfDigits {
 
-
-    public static int extracted(int number) {
+    public static int digitsSum(int number) {
         int sum = 0;
         while (number != 0) {
             int rest = number % 10;
             sum = sum + rest;
             number = number / 10;
-
         }
         return sum;
+
     }
 
 }
--- src/test/java/org/example/P20_SumOfDigitsTest.java
+++ src/test/java/org/example/P20_SumOfDigitsTest.java
@@ -8,8 +8,10 @@ class P20_SumOfDigitsTest {
 
     @Test
     void digitsSum() {
-        int number = 222;
-        int sum = P20_SumOfDigits.extracted(number);
-        assertEquals(6, sum);
+
+        int numb = 135;
+        int sum = P20_SumOfDigits.digitsSum(numb);
+        assertEquals(9, sum);
+
     }
 }
