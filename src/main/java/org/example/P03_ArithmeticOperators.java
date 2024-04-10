b822ab0492b4beae19b267d4059305bec79f3880
DeedeeDL <diana_girl_89@yahoo.com>
2024-04-10 10:42:21 +0300

    update

--- src/main/java/org/example/P03_ArithmeticOperators.java
+++ src/main/java/org/example/P03_ArithmeticOperators.java
@@ -1,8 +1,8 @@
 package org.example;
 
 public class P03_ArithmeticOperators {
-        public static int arithmetic() {
-        int result = 9+6-3*4/2%5;
+    public static int arithmetic() {
+        int result = 9 + 6 - 3 * 4 / 2 % 5;
         return result;
 
     }
